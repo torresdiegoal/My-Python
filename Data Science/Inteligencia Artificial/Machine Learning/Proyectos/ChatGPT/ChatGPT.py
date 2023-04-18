372edd0ef56ad388738648a8e2a505bd7efc66e1
torresdiegoal <diegoa.torresr@outlook.com>
2023-04-18 15:53:02 -0500

    c1

--- Data Science/Inteligencia Artificial/Machine Learning/Proyectos/ChatGPT/ChatGPT.py
+++ Data Science/Inteligencia Artificial/Machine Learning/Proyectos/ChatGPT/ChatGPT.py
@@ -2,3 +2,6 @@ import openai
 
 openai.Completion.create(engine='text-davinci-003',
                          prompt='')
+
+
+# C:/Users/Diego Torres/.vscode/extensions/equinusocio.vsc-community-material-theme-1.4.6/themes/Community-Material-Theme-Darker-High-Contrast.json
