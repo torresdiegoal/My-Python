84fbda336ed89df176b365df9334f6e85fa5c657
torresdiegoal <diegoa.torresr@outlook.com>
2024-07-29 11:14:53 -0500

    c1

---
+++ Basico/Basico-Adriana/SQL-Ejercicios.sql
@@ -0,0 +1,96 @@
+--SQL NIVEL BASICO CONSULTAS--
+--Selecciona todas las columnas de la tabla Person.Person.
+use AdventureWorks2022
+
+SELECT * 
+FROM Person.Person
+
+--Selecciona los productos de la tabla Production.Product cuyo precio de lista (ListPrice) sea mayor a $500.
+SELECT Name, ListPrice
+FROM Production.Product
+WHERE ListPrice > 500
+ORDER BY ListPrice ASC
+
+--Ordena los clientes (Sales.Customer) por apellido (LastName) en orden descendente.
+SELECT sc.CustomerID AS ClienteID,
+p.FirstName AS Nombre,
+p.LastName AS Apellido
+FROM Sales.Customer sc
+JOIN Person.Person p ON sc.PersonID = p.BusinessEntityID
+ORDER BY p.LastName DESC
+
+--Cuenta cuántos productos hay en cada subcategoría (Production.ProductSubcategory).
+
+SELECT 
+ps.name AS NameSubCategory,
+COUNT(p.ProductID) AS Cantidad
+FROM Production.ProductSubcategory ps
+LEFT JOIN Production.Product p
+ON ps.ProductSubcategoryID = p.ProductSubcategoryID
+GROUP BY ps.Name
+
+--Muestra el nombre del cliente, el nombre del producto y la fecha del pedido para todos los pedidos realizados.
+SELECT sc.CustomerID AS ClienteID,
+p.FirstName AS Nombre,
+p.LastName AS Apellido,
+pr.Name AS NombreProducto,
+soh.ShipDate AS FechasPedido
+FROM Sales.Customer sc
+LEFT JOIN Sales.SalesOrderHeader soh ON sc.CustomerID=soh.CustomerID
+LEFT JOIN Sales.SalesOrderDetail sod ON soh.SalesOrderID=sod.SalesOrderID
+LEFT JOIN Production.Product pr ON sod.ProductID = pr.ProductID
+JOIN Person.person p ON sc.PersonID = p.BusinessEntityID
+
+--Calcula el precio promedio de los productos en cada subcategoría.
+SELECT ProductSubcategoryID, AVG(StandardCost) AS PrecioPromedio
+FROM Production.Product 
+GROUP BY ProductSubcategoryID
+
+--Muestra los productos que tienen un precio de lista superior al precio promedio de todos los productos.
+SELECT Name AS NombreProducto, ListPrice
+FROM Production.Product
+WHERE ListPrice > (SELECT AVG(ListPrice) FROM Production.Product); --subconsulta dentro de la consulta principal
+
+--Crea una vista que muestre el nombre del cliente, el número total de pedidos y el gasto total.
+CREATE VIEW vw_clientegasto AS
+SELECT sc.CustomerID AS ClienteID,
+p.FirstName AS Nombre,
+p.LastName AS Apellido,
+OrderQty AS Cantidad, 
+UnitPrice*OrderQty As Precio
+FROM Sales.Customer sc 
+LEFT JOIN Sales.SalesOrderHeader soh ON sc.CustomerID=soh.CustomerID
+LEFT JOIN Sales.SalesOrderDetail sod ON soh.SalesOrderID=sod.SalesOrderID
+JOIN Person.Person p ON sc.PersonID = p.BusinessEntityID
+
+SELECT * FROM vw_clientegasto
+
+--Calcular el porcentaje de ventas de cada producto respecto al total de ventas de su categoría.
+
+WITH PorcentajeVentas AS(
+    SELECT pc.ProductCategoryID AS CategoriaID,
+	pc.Name AS NombreProducto,
+	SUM(pos.UnitPrice*pos.OrderQty) AS Porcentaje
+	FROM Production.ProductCategory pc
+	LEFT JOIN Production.ProductSubcategory ps ON pc.ProductCategoryID = ps.ProductCategoryID
+	LEFT JOIN Production.Product p ON p.ProductSubcategoryID= ps.ProductSubcategoryID
+	LEFT JOIN Sales.SalesOrderDetail pos ON pos.ProductID = p.ProductID
+	GROUP BY pc.ProductCategoryID, pc.Name, p.Name, p.ProductID
+	),
+
+	PorcentajeCategoria AS (
+	SELECT CategoriaID AS Categoria,
+	SUM (Porcentaje) AS VentasCategoria
+	FROM PorcentajeVentas
+	GROUP BY CategoriaID
+	)
+
+SELECT
+  pv.CategoriaID,
+  pv.NombreProducto,
+  pv.Porcentaje,
+  pc.VentasCategoria,
+  (pv.Porcentaje / pc.VentasCategoria) * 100 AS PorcentajeTotal
+FROM PorcentajeVentas pv
+JOIN PorcentajeCategoria pc ON pv.CategoriaID = pc.Categoria
+ORDER BY pv.CategoriaID, pv.Porcentaje DESC;
--- Basico/Basico-Adriana/SQL.sql
+++ Basico/Basico-Adriana/SQL.sql
@@ -69,3 +69,121 @@ ORDER BY BusinessEntityID DESC;
 SELECT NationalIDNumber, JobTitle
 FROM HumanResources.Employee
 WHERE JobTitle NOT LIKE 'Research%';
+
+SELECT AddressLine1,AddressLine2
+FROM Person.Address
+WHERE AddressLine2 IS NOT NULL;
+
+SELECT MIN (PostalCode) 
+FROM Person.Address;
+--FUNCIONES DE AGREGACION
+SELECT COUNT (NationalIDNumber) --Cuenta valores distintos al nulo
+FROM HumanResources.Employee
+
+SELECT SUM (TaxRate)
+FROM Sales.SalesTaxRate;
+
+SELECT AVG (TaxRate)
+FROM Sales.SalesTaxRate;
+
+SELECT CardType 
+FROM Sales.CreditCard
+WHERE ExpMonth IN ('11')
+
+SELECT COUNT(Name)
+FROM HumanResources.Department
+
+SELECT SUM(StandardCost)
+FROM Production.ProductCostHistory
+WHERE ProductID IN ('718')
+
+SELECT AVG(StandardCost) AS "Promedio"
+FROM Production.ProductCostHistory
+WHERE ProductID BETWEEN 716 AND 720
+
+SELECT CONCAT ("FirstName",' ' , "LastName") AS "Nombre Completo"
+FROM Person.Person
+WHERE PersonType LIKE ('VC')
+
+--GroupBy a que nivel lo agrego y a que lo agrego
+SELECT ProductID, AVG(StandardCost) AS "Standard Cost Max"
+FROM Production.ProductCostHistory
+GROUP BY ProductID
+--A que lo agrego y que agrego
+SELECT ProductID, MIN(StandardCost) AS "Standard Cost Min"
+FROM Production.ProductCostHistory
+GROUP BY ProductID
+-- It tells the database to group the rows by the values in the CountryRegionCode column. 
+--For each unique country/region code, the query will then calculate the COUNT(Name) for that group.
+SELECT CountryRegionCode, COUNT(Name) AS "CountProvince"
+FROM Person.StateProvince
+GROUP BY CountryRegionCode
+
+SELECT BusinessEntityID, AVG(Rate) AS "AVG-Rate"
+FROM HumanResources.EmployeePayHistory
+GROUP BY BusinessEntityID;
+
+WITH Temporal1 AS (
+	SELECT BusinessEntityID, AVG(Rate) AS AVG_Rate
+	FROM HumanResources.EmployeePayHistory	
+	GROUP BY BusinessEntityID)
+
+SELECT *
+FROM Temporal1
+WHERE AVG_Rate > 43
+ORDER BY AVG_Rate DESC
+
+--HAVING SIRVE PARA LAS AGREGACIONES EN VEZ DE USAR EL WHERE Y HACER TEMPORALES
+
+SELECT BusinessEntityID, AVG(Rate) AS AVG_Rate
+FROM HumanResources.EmployeePayHistory	
+GROUP BY BusinessEntityID
+HAVING AVG(Rate) > 43 
+ORDER BY AVG_Rate ASC
+
+SELECT PostalCode,
+CASE 
+	WHEN PostalCode = '98055'
+	THEN AddressLine1
+	ELSE NULL END as Address
+FROM Person.Address 
+
+ --CASO REALISTA DE CASE
+--SELECT
+--	  C.T_CONTACT_ID,
+--      C.CONTACT_NUMBER,
+--      C.FIRSTNAME, 
+--      C.LASTNAME,
+--      C.ID_NUMBER,
+--      C.BIRTHDATE AS FECHA_DE_NACIMIENTO,
+--      -- OBTIENE LA EDAD EN AÑOS BASADA EN LA DIFERENCIA ENTRE LA FECHA ACTUAL Y LA FECHA DE NACIMIENTO
+--      CASE 
+--            WHEN C.BIRTHDATE IS NULL THEN NULL
+--            ELSE FLOOR(DATEDIFF(MONTH, C.BIRTHDATE, GETDATE()) / 12)
+--        END AS EDAD,
+--        CASE 
+--            WHEN FLOOR(DATEDIFF(MONTH, C.BIRTHDATE, GETDATE()) / 12) IS NULL THEN 'SIN DATO'
+--            WHEN FLOOR(DATEDIFF(MONTH, C.BIRTHDATE, GETDATE()) / 12) < 18 THEN 'MENOR DE 18'
+--            WHEN FLOOR(DATEDIFF(MONTH, C.BIRTHDATE, GETDATE()) / 12) <= 24 THEN '18 A 24'
+--            WHEN FLOOR(DATEDIFF(MONTH, C.BIRTHDATE, GETDATE()) / 12) <= 34 THEN '25 A 34'
+--            WHEN FLOOR(DATEDIFF(MONTH, C.BIRTHDATE, GETDATE()) / 12) <= 44 THEN '35 A 44'
+--            WHEN FLOOR(DATEDIFF(MONTH, C.BIRTHDATE, GETDATE()) / 12) <= 54 THEN '45 A 54'
+--            WHEN FLOOR(DATEDIFF(MONTH, C.BIRTHDATE, GETDATE()) / 12) <= 64 THEN '55 A 64'
+--            ELSE 'MAYOR A 64'
+--        END AS RANGO_DE_EDAD
+
+SELECT AddressLine2,
+ISNULL(AddressLine2,'N/A')
+FROM Person.Address
+
+--INNER JOIN--
+SELECT *
+FROM HumanResources.Employee
+INNER JOIN HumanResources.EmployeeDepartmentHistory
+ON HumanResources.EmployeeDepartmentHistory.BusinessEntityID = HumanResources.Employee.BusinessEntityID 
+
+--Obtén una lista de todos los productos junto con el nombre de su subcategoría
+SELECT p.Name AS NombreProducto, ps.Name AS Subcategory --Selecciono los nombres de los productos y sibcategorias que deseo
+FROM Production.Product p --La tabla de donde deseo sacar la informacion
+INNER JOIN Production.ProductSubcategory ps --La tabla con la que la comparo
+ON p.ProductSubcategoryID = ps.ProductSubcategoryID --La condicion de comparacion
